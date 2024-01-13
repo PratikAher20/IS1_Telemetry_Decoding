3a16cf02d58e9861f5ef892ff653c9b2d8a7dab6
Pratik Aher <106163261+PratikAher20@users.noreply.github.com>
2024-01-13 12:05:58 +0530

    Commit after Making the Code storage Efficient
    Now the Code only runs when there is a new file in the source folder. Overall Data  starting from the Launch is stored in different folder and is overwriting repeatedly.

--- Inspire_Telemetry_Decoder_v6.py
+++ Inspire_Telemetry_Decoder_v6.py
@@ -1,5 +1,10 @@
 import csv
 import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler
+from threading import Thread
 from tkinter import filedialog
 from tkinter import *
 from tkinter import ttk
@@ -7,8 +12,11 @@ from satnogs.satnogs import Satnogs
 import cubeds
 import argparse
 import datetime
+import subprocess
+import sys
 from datetime import timedelta
 from threading import Timer
+from threading import Thread
 import time
 
 LARGE_FONT= ("Verdana", 12)
@@ -91,13 +99,13 @@ def loadPacketDefs():
             packets_def.append(list(list_temp))
     return packets_def
 
-def loadRawDataUser():
+def loadRawDataUser(New_P):
     array_to_return = []
     raw_list = []
     # Opening multiple hydra log files
-    root = Tk()
-    root.withdraw()
-    Path = filedialog.askdirectory()
+    # root = Tk()
+    # root.withdraw()
+    Path = New_P
     array_to_return.append(Path)
     total_files = 0
     for path, subdirs, filelist in os.walk(Path):
@@ -120,8 +128,10 @@ def loadRawDataUser():
 def loadRawDataAutomated():
     array_to_return = []
     raw_list = []
-    Path = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data"
-    array_to_return.append(r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Processed_Data")
+    #D:\INSPIRESat-1 Data Server\Data_Server\IS1 On-Orbit Data\Raw Data
+    Path = r"D:\SSAPCE_Lab_Material\Inspire_Telemetry_Decoder_v7\test"
+    # D:\INSPIRESat-1 Data Server\Data_Server\IS1 On-Orbit Data\Processed data
+    array_to_return.append(r"D:\SSAPCE_Lab_Material\Inspire_Telemetry_Decoder_v7\Processed_Data")
     total_files = 0
     for path, subdirs, filelist in os.walk(Path):
         for name in filelist:
@@ -140,38 +150,29 @@ def loadRawDataAutomated():
     array_to_return.append(out_file_prefix)
     return array_to_return
 
-def loadlatestRawDataAutomated():
+def loadTriggeredRawDataAutomated(event):
     array_to_return = []
     raw_list = []
-    Path = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data"
-    array_to_return.append(r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Processed_Data")
     total_files = 0
-    most_recent_file = None
-    most_recent_time = 0
-    for path, subdirs, filelist in os.scandir(Path):
+    array_to_return.append(r"D:\SSAPCE_Lab_Material\Inspire_Telemetry_Decoder_v7\Processed_Data")
+    for path, dirs, filelist in os.walk(event.src_path, topdown=True):
         for name in filelist:
             total_files += 1
-            if filelist.is_file():
-                # Checking the modification time of the file
-                mod_time = filelist.stat().st_mtime_ns
-                if mod_time > most_recent_time:
-                    # update the most recent file and its modification time
-                    most_recent_file = filelist.name
-                    most_recent_time = mod_time
+            # if os.path.isfile(path + "/" + name):
             with open(path + "/" + name, 'rb') as f:
                 byte = f.read(1)
                 while byte:
                     raw_list.append(int(ord(byte)))
                     byte = f.read(1)
     array_to_return.append(raw_list)
-    #If only one raw file is present, add filename as prefix to output
-    if (total_files==1):
+    # If only one raw file is present, add filename as prefix to output
+    if (total_files == 1):
         out_file_prefix = filelist[0] + "_"
     else:
         out_file_prefix = ""
     array_to_return.append(out_file_prefix)
     return array_to_return
-
+    # Path = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data"
 
 #Parse and decode packets
 def parseanddecode(list_packets,packets_def,raw_data_array):
@@ -183,6 +184,7 @@ def parseanddecode(list_packets,packets_def,raw_data_array):
     pkt_cnt = 1
     actual_pckt_len = 0
     check_flag = 0
+    curr_decoded_array_index = 0
 
     #Extracting level-0 packets from raw data
     while (array_index + 5 < len(raw_list)):
@@ -196,7 +198,7 @@ def parseanddecode(list_packets,packets_def,raw_data_array):
 
         for j in range(0, len(list_packets), 1):
             if (packet_apid == int(list_packets[j][1]) and packet_length == int(list_packets[j][2])):
-                print("Header", packet_header, " Number", pkt_cnt, "APID", packet_apid, "Length", packet_length)
+                #print("Header", packet_header, " Number", pkt_cnt, "APID", packet_apid, "Length", packet_length)
                 list_packets[j][3].append(list(raw_list[array_index:array_index + packet_length + 7]))  # 7
                 actual_pckt_len = int(list_packets[j][2])
                 check_flag = 0
@@ -232,7 +234,7 @@ def parseanddecode(list_packets,packets_def,raw_data_array):
             curr_packet_decode_number = a
             curr_packet_header_array = []
             curr_packet_decoded_array = []
-            curr_decoded_array_index = 0
+            # curr_decoded_array_index = 0
 
             for i in range(0, len(curr_packet_raw_array), 1):
 
@@ -241,7 +243,6 @@ def parseanddecode(list_packets,packets_def,raw_data_array):
 
                 for j in range(0, len(curr_packet_def), 1):
                     # Implementing decoding - combining bytes
-                    print(curr_packet_def[j][0])
                     type = curr_packet_def[j][2]
                     conversion = curr_packet_def[j][4:9]
                     # print(conversion)
@@ -381,12 +382,46 @@ def parseanddecode(list_packets,packets_def,raw_data_array):
 
     return list_packets
 
+def storeOverallDecodedPackets(list_packets, raw_data_array):
+    # print("bhai mai kam kr rha hu")
+    Path = raw_data_array[0]
+    out_file_prefix = raw_data_array[2]
+    # Creating main directory with date and time
+    main_directory = "Overall_data"
+    path_new_main = os.path.join(Path, main_directory)
+    subprocess.run([r"D:\SSAPCE_Lab_Material\Inspire_Telemetry_Decoder_v7\auto.bat"])
+    os.mkdir(path_new_main)
+    # creating new level 0 and level 1 folders which would contain the decoded files
+    l0_directory = "Level 0 Packets"
+    path_new_l0 = os.path.join(path_new_main, l0_directory)
+    os.mkdir(path_new_l0)
+
+    l1_directory = "Level 1 Packets"
+    path_new_l1 = os.path.join(path_new_main, l1_directory)
+    os.mkdir(path_new_l1)
+
+    # writing the raw different types of packets to level 0 csv files
+    for j in range(0, len(list_packets), 1):
+        if (len(list_packets[j][3]) > 0):
+            name_str = out_file_prefix + str(list_packets[j][0]) + "_level_0.csv"
+            with open(path_new_l0 + "/" + name_str, "w") as f:
+                writer = csv.writer(f)
+                for row in list_packets[j][3]:
+                    writer.writerow(row)
+            # Store the level 1 data in CSV files
+            name_str_l1 = out_file_prefix + str(list_packets[j][0]) + "_level_1.csv"
+            with open(path_new_l1 + "/" + name_str_l1, "w", newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(list_packets[j][6])
+                for row in list_packets[j][4]:
+                    writer.writerow(row)
+
 def storeDecodedPackets(list_packets, raw_data_array):
 
     Path = raw_data_array[0]
     out_file_prefix = raw_data_array[2]
     # Creating main directory with date and time
-    main_directory = "Decoded_Packets_" + str(int(get_current_date_time().strftime("%d%m%Y_%H%M%S")))
+    main_directory = "Decoded_Packets_" + str(get_current_date_time().strftime("%d%m%Y_%H%M%S"))
     path_new_main = os.path.join(Path, main_directory)
     os.mkdir(path_new_main)
     # creating new level 0 and level 1 folders which would contain the decoded files
@@ -434,70 +469,39 @@ def batchDecodePackets():
     popupmsg("Success", "Done! Level 0 and Level 1 Packets Created")
 
 
-def automatedDecode():
-    # Load Packet APIDs
-    print("Running Automation")
-    list_packets = loadPacketAPIDs()
-    # Reading the packet definations from the  packet_def.csv file
-    packets_def = loadPacketDefs()
-    # Empty Satnogs directory
-    SatNogs_dir = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data\SatNogs"
-    for f in os.listdir(SatNogs_dir):
-        os.remove(os.path.join(SatNogs_dir, f))
-    # Download Latest Satnogs Data
-    #downloadSatnogsFile()
-    # Load Row Data Automated
-    raw_data_array = loadRawDataAutomated()
-    # Parse and Decode Packets
-    list_packets_decoded = parseanddecode(list_packets, packets_def, raw_data_array)
-    # Store Decoded Packets
-    storeDecodedPackets(list_packets_decoded, raw_data_array)
-    #Calling Schedular for next day
-    automateDecodeSchedule()
-    return
+# def automatedDecode():
+#     # Load Packet APIDs
+#     print("Running Automation")
+#     list_packets = loadPacketAPIDs()
+#     # Reading the packet definations from the  packet_def.csv file
+#     packets_def = loadPacketDefs()
+#     # Empty Satnogs directory
+#     SatNogs_dir = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data\SatNogs"
+#     for f in os.listdir(SatNogs_dir):
+#         os.remove(os.path.join(SatNogs_dir, f))
+#     # Download Latest Satnogs Data
+#     #downloadSatnogsFile()
+#     # Load Row Data Automated
+#     raw_data_array = loadRawDataAutomated()
+#     # Parse and Decode Packets
+#     list_packets_decoded = parseanddecode(list_packets, packets_def, raw_data_array)
+#     # Store Decoded Packets
+#     storeDecodedPackets(list_packets_decoded, raw_data_array)
+#     #Calling Schedular for next day
+#     automateDecodeSchedule()
+#     return
 
-def latestautomatedDecode():
-    # Load Packet APIDs
-    print("Running Automation")
-    list_packets = loadPacketAPIDs()
-    # Reading the packet definations from the  packet_def.csv file
-    packets_def = loadPacketDefs()
-    # Empty Satnogs directory
-    SatNogs_dir = r"C:\Users\Anant\Desktop\On-Orbit_Data_03032022\On-Orbit_Raw_Data\SatNogs"
-    for f in os.listdir(SatNogs_dir):
-        os.remove(os.path.join(SatNogs_dir, f))
-    # Download Latest Satnogs Data
-    #downloadSatnogsFile()
-    # Load Row Data Automated
-    raw_data_array = loadlatestRawDataAutomated()
-    # Parse and Decode Packets
-    list_packets_decoded = parseanddecode(list_packets, packets_def, raw_data_array)
-    # Store Decoded Packets
-    storeDecodedPackets(list_packets_decoded, raw_data_array)
-    #Calling Schedular for next day
-    automateDecodeSchedule()
-    return
 #Function to schedule and run automated decoding
-def automateDecodeSchedule():
-    #TODO Decide Time and Set Here
-    x = datetime.datetime.today()
-    y = x.replace(day=x.day, hour=7, minute=30, second=0, microsecond=0) + timedelta(hours=24)
-    delta_t = y - x
-
-    secs = delta_t.total_seconds()
-
-    t = Timer(secs, automatedDecode)
-    t.start()
-
-def latest_automateDecodeSchedule():
-    x = datetime.datetime.today()
-    y = x.replace(day=x.day, hour=7, minute=30, second=0, microsecond=0) + timedelta(hours=24)
-    delta_t = y - x
-
-    secs = delta_t.total_seconds()
-
-    t = Timer(secs, automatedDecode)
-    t.start()
+# def automateDecodeSchedule():
+#     #TODO Decide Time and Set Here
+#     x = datetime.datetime.today()
+#     y = x.replace(day=x.day, hour=7, minute=30, second=0, microsecond=0) + timedelta(hours=24)
+#     delta_t = y - x
+#
+#     secs = delta_t.total_seconds()
+#
+#     t = Timer(secs, automatedDecode)
+#     t.start()
 
 # -------------- HELPER FUNCTIONS FOR THIS SATNOGS DOWNLOAD FUNCTION----------------------------------------------------
 def parse_command_line_args():
@@ -558,51 +562,91 @@ def About():
     popupmsg("About","INSPIRE Telemetry Decoder Version 6, Created by: Anant "
                      "\nSource Code: https://github.com/anant-infinity/IS1_Temeletry_Decoding.git")
 
-root = Tk()
-root.iconbitmap(default='inspire_logo_icon.ico')
-root.title("INSPIRE Telemetry Decoder v6")
-menu = Menu(root)
-root.config(menu=menu)
-filemenu = Menu(menu)
-menu.add_cascade(label="Decode", menu=filemenu)
-filemenu.add_command(label="Select Folder and Decode Files", command=batchDecodePackets)
-
-downloadmenu = Menu(menu)
-menu.add_cascade(label="Download Raw Files", menu=downloadmenu)
-downloadmenu.add_command(label="Download Satnogs Raw File", command=userDownloadSatnogsFile)
-
-automenu = Menu(menu)
-menu.add_cascade(label="Automation", menu=automenu)
-automenu.add_command(label="Automated Decode Latest Packets", command=automateDecodeSchedule)
-
-automenu = Menu(menu)
-menu.add_cascade(label="Latest_Automation", menu=automenu)
-automenu.add_command(label="Automated Decode Latest Packets", command=latest_automateDecodeSchedule)
-
-helpmenu = Menu(menu)
-menu.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=About)
-
-
-text1 = Text(root, height=30, width=75)
-photo = PhotoImage(file='./INSPIRE_logo.png')
-text1.insert(END, '\n')
-text1.image_create(END, image=photo)
-text1.pack(side=LEFT)
+def automatedDecode():
+    # t1 = Thread()
+    # t1.start()
+    list_packets = loadPacketAPIDs()
+    packets_def = loadPacketDefs()
+    raw_data_array = loadRawDataAutomated()
+    list_packets_decoded = parseanddecode(list_packets, packets_def, raw_data_array)
+    storeOverallDecodedPackets(list_packets_decoded, raw_data_array)
+    print("Overll_Data_Updated")
+    # t1.join()
+    # automatedDecode()
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            list_packets = loadPacketAPIDs()
+            packets_def = loadPacketDefs()
+            raw_data_array = loadTriggeredRawDataAutomated(event)
+            list_packets_decoded = parseanddecode(list_packets, packets_def, raw_data_array)
+            storeDecodedPackets(list_packets_decoded, raw_data_array)
+            for path, dirs, filelist in os.walk(event.src_path, topdown=True):
+                for name in filelist:
+                    print("Decoded file: - ", filelist[0])
+                    break
+            automatedDecode()
 
 
-text2 = Text(root, height=30, width=70)
-scroll = Scrollbar(root, command=text2.yview)
-text2.configure(yscrollcommand=scroll.set)
-text2.tag_configure('bold_italics', font=('Verdana', 12, 'bold', 'italic'))
-text2.tag_configure('big', font=('Verdana', 16, 'bold'))
-text2.tag_configure('color',
-                    foreground='#476042',
-                    font=('Verdana', 12, 'bold'))
-text2.tag_bind('follow',
-               '<1>',
-               lambda e, t=text2: t.insert(END, "Not now, maybe later!"))
-text2.insert(END,'\nHow to Run\n', 'big')
+print("Running Automation")
+# automatedDecode()
+observer = Observer()
+event_handler = MyHandler()
+observer.schedule(event_handler, path=r'D:\SSAPCE_Lab_Material\Inspire_Telemetry_Decoder_v7\test', recursive=True)
+observer.start()
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+
+
+# root = Tk()
+# root.iconbitmap(default='inspire_logo_icon.ico')
+# root.title("INSPIRE Telemetry Decoder v6")
+# menu = Menu(root)
+# root.config(menu=menu)
+# filemenu = Menu(menu)
+# menu.add_cascade(label="Decode", menu=filemenu)
+# filemenu.add_command(label="Select Folder and Decode Files", command=batchDecodePackets)
+#
+# downloadmenu = Menu(menu)
+# menu.add_cascade(label="Download Raw Files", menu=downloadmenu)
+# downloadmenu.add_command(label="Download Satnogs Raw File", command=userDownloadSatnogsFile)
+#
+# automenu = Menu(menu)
+# menu.add_cascade(label="Automation", menu=automenu)
+# automenu.add_command(label="Automated Decode Latest Packets", command=loadRawDataAutomated())
+
+# # automenu = Menu(menu)
+# # menu.add_cascade(label="Latest_Automation", menu=automenu)
+# # automenu.add_command(label="Automated Decode Latest Packets", command=latest_automateDecodeSchedule)
+#
+# helpmenu = Menu(menu)
+# menu.add_cascade(label="Help", menu=helpmenu)
+# helpmenu.add_command(label="About...", command=About)
+
+
+# text1 = Text(root, height=30, width=75)
+# photo = PhotoImage(file='./INSPIRE_logo.png')
+# text1.insert(END, '\n')
+# text1.image_create(END, image=photo)
+# text1.pack(side=LEFT)
+#
+#
+# text2 = Text(root, height=30, width=70)
+# scroll = Scrollbar(root, command=text2.yview)
+# text2.configure(yscrollcommand=scroll.set)
+# text2.tag_configure('bold_italics', font=('Verdana', 12, 'bold', 'italic'))
+# text2.tag_configure('big', font=('Verdana', 16, 'bold'))
+# text2.tag_configure('color',
+#                     foreground='#476042',
+#                     font=('Verdana', 12, 'bold'))
+# text2.tag_bind('follow',
+#                '<1>',
+#                lambda e, t=text2: t.insert(END, "Not now, maybe later!"))
+# text2.insert(END,'\nHow to Run\n', 'big')
 quote = """
 This application can be used to generate 
 Level 0 and Level 1 telemetry.Follow the 
@@ -622,10 +666,10 @@ Further instructions of setting up are
 provided in the ReadMe.
 
 """
-text2.insert(END, quote, 'color')
-text2.pack(side=LEFT)
-scroll.pack(side=RIGHT, fill=Y)
-mainloop()
+# text2.insert(END, quote, 'color')
+# text2.pack(side=LEFT)
+# scroll.pack(side=RIGHT, fill=Y)
+# mainloop()
 
 
 
